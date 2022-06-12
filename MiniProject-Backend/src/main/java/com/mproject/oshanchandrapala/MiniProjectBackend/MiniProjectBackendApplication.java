1278a11f9b4729c7584e25caab27cd130b7b74c1
oshanvikasith <oshanvikasith1998@gmail.com>
2022-06-12 15:56:07 +0530

    initial commit to category feature branch

--- MiniProject-Backend/src/main/java/com/mproject/oshanchandrapala/MiniProjectBackend/MiniProjectBackendApplication.java
+++ MiniProject-Backend/src/main/java/com/mproject/oshanchandrapala/MiniProjectBackend/MiniProjectBackendApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniProjectBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MiniProjectBackendApplication.class, args);
+
 	}
 
 }
