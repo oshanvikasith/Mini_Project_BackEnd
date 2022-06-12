db735411dedcbfce0b35f5550d7bdd92b3274a24
oshanvikasith <oshanvikasith1998@gmail.com>
2022-06-12 11:25:51 +0530

    Initial project commit

---
+++ MiniProject-Backend/src/test/java/com/mproject/oshanchandrapala/MiniProjectBackend/MiniProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mproject.oshanchandrapala.MiniProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
