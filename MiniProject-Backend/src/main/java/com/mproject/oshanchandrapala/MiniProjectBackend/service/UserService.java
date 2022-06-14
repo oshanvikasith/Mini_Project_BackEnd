6f5a0130f5d404b77acacb8514cc457ecbb690f9
oshanvikasith <oshanvikasith1998@gmail.com>
2022-06-14 10:20:02 +0530

    User feature implementation

--- MiniProject-Backend/src/main/java/com/mproject/oshanchandrapala/MiniProjectBackend/service/UserService.java
+++ MiniProject-Backend/src/main/java/com/mproject/oshanchandrapala/MiniProjectBackend/service/UserService.java
@@ -2,20 +2,28 @@ package com.mproject.oshanchandrapala.MiniProjectBackend.service;
 
 import com.mproject.oshanchandrapala.MiniProjectBackend.model.User;
 import com.mproject.oshanchandrapala.MiniProjectBackend.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
 
     private final UserRepo userRepo;
 
+    @Autowired
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
     }
 
     public Optional<User> findUserByEmail(String userEmail){
         return userRepo.findUserByEmail(userEmail);
+    }
+
+    public User addUser(User user){
+        user.setUserId(UUID.randomUUID().toString());
+        return userRepo.save(user);
     }
 }
